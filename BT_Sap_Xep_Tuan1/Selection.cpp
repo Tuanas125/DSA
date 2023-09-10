08c4de99d3be5c8ae7b59f5776bc3c2a7b30971d
Tuanas125 <113823199+Tuanas125@users.noreply.github.com>
2023-09-10 13:01:08 +0700

    Add files via upload

---
+++ BT_Sap_Xep_Tuan1/Selection.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
+
+int a[102];
+
+int main(){
+    //freopen("square.in", "r", stdin);
+    int n; cin >> n;
+
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    for(int i = 0; i < n - 1; i++) {
+        int jMin = i;
+        for(int j = i+1; j < n; j++) {
+            if(a[j] < a[jMin]) {
+                jMin = j;
+            }
+        }
+
+        if(jMin != i) {
+            swap(a[i], a[jMin]);
+        }
+    }
+
+    for(int i = 0; i < n; i++) cout << a[i];
+}
