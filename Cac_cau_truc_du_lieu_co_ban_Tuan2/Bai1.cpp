e39be3499ec1cb82a3f275386e4ca90cb0d0bfa2
Tuanas125 <113823199+Tuanas125@users.noreply.github.com>
2023-09-16 16:16:38 +0700

    Add exercise1

---
+++ Cac_cau_truc_du_lieu_co_ban_Tuan2/Bai1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
+
+int a[102];
+
+int main(){
+    freopen("square.in", "r", stdin);
+    int n, ans = 0; cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(a[i] == a[j]) {
+                ans++;
+            }
+        }
+    }
+    cout << ans;
+}
